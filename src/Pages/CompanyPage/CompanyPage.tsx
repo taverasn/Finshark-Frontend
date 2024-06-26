93e8a9daff70187e09d822e29fe9ac172fd67674
Nicholas Taveras <taveras123n@gmail.com>
2024-06-26 18:55:32 -0400

    Number Formatting

--- src/Pages/CompanyPage/CompanyPage.tsx
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -7,6 +7,7 @@ import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard
 import Tile from "../../Components/Tile/Tile";
 import Spinner from "../../Components/Spinner/Spinner";
 import TenKFinder from "../../Components/TenKFinder/TenKFinder";
+import { formatRatio } from "../../Helpers/NumberFormatting";
 
 interface Props {}
 
@@ -29,9 +30,15 @@ const CompanyPage = (props: Props) => {
           <Sidebar />
           <CompanyDashboard ticker={ticker!}>
             <Tile title="Company Name" subTitle={company.companyName} />
-            <Tile title="Price" subTitle={company.price.toString()} />
+            <Tile
+              title="Price"
+              subTitle={"$" + formatRatio(company.price).toString()}
+            />
+            <Tile
+              title="DCF"
+              subTitle={"$" + formatRatio(company.dcf).toString()}
+            />
             <Tile title="Sector" subTitle={company.sector} />
-            <Tile title="DCF" subTitle={company.dcf.toString()} />
             <TenKFinder ticker={company.symbol} />
             <p className="br-white shadow rounded text-medium text-gray-900 p-3 mt-1 m-4">
               {company.description}
