002f06173670c1560beee11d079dac32b2fc072e
Nicholas Taveras <taveras123n@gmail.com>
2024-06-26 00:27:04 -0400

    Balance Sheet Added to Company Profile Page

--- src/Components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
+++ src/Components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
@@ -9,7 +9,7 @@ const DeletePortfolio = ({ onPortfolioDelete, portfolioValue }: Props) => {
   return (
     <div>
       <form onSubmit={onPortfolioDelete}>
-        <input hidden={true} value={portfolioValue} />
+        <input readOnly={true} hidden={true} value={portfolioValue} />
         <button className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-red-500 hover:text-red-500 hover:bg-white border-red-500">
           X
         </button>
--- src/Components/RatioList/RatioList.tsx
+++ src/Components/RatioList/RatioList.tsx
@@ -5,16 +5,22 @@ interface Props {
 }
 
 const RatioList = ({ config, data }: Props) => {
-  const renderedRows = config.map((row: any) => {
+  const renderedCells = config.map((row: any) => {
     return (
-      <li className="py-3 sm:py-4" key={uuidv4()}>
+      <li className="py-6 sm:py-6" key={uuidv4()}>
         <div className="flex items-center space-x-4">
           <div className="flex-1 min-w-0">
-            <p className="text-sm font-medium text-gray-900 truncate">
+            <div className="text-sm font-medium text-gray-900 truncate">
               {row.label}
-            </p>
+            </div>
             <p className="text-sm text-gray-500 truncate">
-              {row.subTitle && row.subTitle}
+              <a
+                href="/cdn-cgi/l/email-protection"
+                className="__cf_email__"
+                data-cfemail="17727a767e7b57607e7973646372653974787a"
+              >
+                {row.subTitle && row.subTitle}
+              </a>
             </p>
           </div>
           <div className="inline-flex items-center text-base font-semibold text-gray-900">
@@ -25,8 +31,8 @@ const RatioList = ({ config, data }: Props) => {
     );
   });
   return (
-    <div className="bg-white shadow rounded-lg ml-4 mt-4 mb-4 p-4 sm:p-6 h-full">
-      <ul className="divide-y divided-gray-200">{renderedRows}</ul>
+    <div className="bg-white shadow rounded-lg ml-4 mt-4 mb-4 p-4 sm:p-6 w-full">
+      <ul className="divide-y divide-gray-200">{renderedCells}</ul>
     </div>
   );
 };
