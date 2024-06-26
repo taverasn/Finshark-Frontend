f2c529994224dc4196a2f04d8a111626903ad5c0
Nicholas Taveras <taveras123n@gmail.com>
2024-06-25 20:52:18 -0400

    Table Component
    - Table Component
      - Currently uses example data to test look of table
    - Design Guide
      - will be used to show various design aspects of the application such as components like the table

---
+++ src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,18 @@
+import Table from "../../Components/Table/Table";
+
+type Props = {};
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+      <h1>Finshark Design Page</h1>
+      <h2>
+        This is FinShark's design page. This is where we will house various
+        design aspects of the app.
+      </h2>
+      <Table />
+    </>
+  );
+};
+
+export default DesignPage;
