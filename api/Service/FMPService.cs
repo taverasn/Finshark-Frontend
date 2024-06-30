d79384d7708f5533178a1f5f6b6e70d3720958de
Nicholas Taveras <taveras123n@gmail.com>
2024-06-30 15:00:45 -0400

    Data Seeding
    - When a User tries to add a stock to their portfolio or a comment to a stock that is net yet in the database, but exists in the Financial Modeling Prep API it will be added to the database

---
+++ api/Service/FMPService.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Stock;
+using api.Interfaces;
+using api.Mappers;
+using api.Models;
+using Newtonsoft.Json;
+
+namespace api.Service
+{
+    public class FMPService : IFMPService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+        public FMPService(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+        }
+        public async Task<Stock> FindStockBySymbolAsync(string symbol)
+        {
+            try {
+                var result = await _httpClient.GetAsync($"http://financialmodelingprep.com/api/v3/profile/{symbol}?apikey={_config["FMPKey"]}");
+                if(result.IsSuccessStatusCode)
+                {
+                    var content = await result.Content.ReadAsStringAsync();
+                    var tasks = JsonConvert.DeserializeObject<FMPStock[]>(content);
+                    var stock = tasks[0];
+                    if(stock != null)
+                    {
+                        return stock.ToStockFromFMP();
+                    }
+                    return null;
+                }
+                return null;
+            } catch (Exception e) {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+    }
+}
