f66667b92798ca6d13f1cfd96992458cec135b20
Nicholas Taveras <taveras123n@gmail.com>
2024-06-29 22:19:57 -0400

    Register User Endpoint

---
+++ api/Migrations/20240630021553_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "08941941-b1d0-4302-aebb-2c7c2f6b3d1f", null, "Admin", "ADMIN" },
+                    { "31aad844-5cd4-4853-a5c6-a6ae23898317", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "08941941-b1d0-4302-aebb-2c7c2f6b3d1f");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "31aad844-5cd4-4853-a5c6-a6ae23898317");
+        }
+    }
+}
