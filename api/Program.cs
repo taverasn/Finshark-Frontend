3253f1f06ba803c7e89dc220339d4a951cca3d15
Nicholas Taveras <taveras123n@gmail.com>
2024-06-30 15:11:52 -0400

    Added Cors

--- api/Program.cs
+++ api/Program.cs
@@ -106,6 +106,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => {
+    x
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    //.WithOrigins("https://localhost:44351))
+    .SetIsOriginAllowed(origin => true);
+});
+
 app.UseAuthentication();
 app.UseAuthorization();
 
